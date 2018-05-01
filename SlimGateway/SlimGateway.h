e5b7ceee02d764d6e483ce526893b396aa846840
Juanjo <juanvillaescusa72@gmail.com>
2018-05-01 09:49:20 +0200

    Initial Commit

---
+++ SlimGateway/SlimGateway.h
@@ -0,0 +1,19 @@
+//
+//  SlimGateway.h
+//  SlimGateway
+//
+//  Created by Juanjo García Villaescusa on 1/5/18.
+//  Copyright © 2018 Juanjo García Villaescusa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlimGateway.
+FOUNDATION_EXPORT double SlimGatewayVersionNumber;
+
+//! Project version string for SlimGateway.
+FOUNDATION_EXPORT const unsigned char SlimGatewayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlimGateway/PublicHeader.h>
+
+
